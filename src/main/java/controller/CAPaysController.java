b360ec75c7a4a75c90b60e1bc2032294b2de65a6
Numagoet <58596447+Numagoet@users.noreply.github.com>
2020-05-05 23:05:20 +0200

    Controllers

---
+++ src/main/java/controller/CAPaysController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import comptoirs.model.dao.StatisticsDao;
+import java.util.Date;
+import javax.inject.Inject;
+import javax.mvc.Controller;
+import javax.mvc.Models;
+import javax.mvc.View;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author numag
+ */
+@Controller
+@Path("CAPays")
+@View("CAPays.jsp")
+public class CAPaysController {
+    
+    @Inject
+    StatisticsDao dao;
+    
+    @Inject
+    Models models;
+    
+    @GET
+    public void show(){
+        Date min=dao.datePlusAncienneCommande();
+        Date max=dao.datePlusRecenteCommande();
+        models.put("minDate",min);
+        models.put("maxDate",max);
+    }
+    
+}
